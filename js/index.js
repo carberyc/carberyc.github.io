6fe29c7a4fbab389dbad99cb211fd52dd4cf5bf7
Carbery, Charlie <CCarbery@eab.com>
2017-05-31 15:38:01 -0400

    Updated js

--- js/index.js
+++ js/index.js
@@ -65,19 +65,20 @@
             }
         }})
         .on('success.form.bv', function(e) {
-            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-                $('#contact_form').data('bootstrapValidator').resetForm();
+        	window.location.replace("http://stackoverflow.com");
+           // $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+               // $('#contact_form').data('bootstrapValidator').resetForm();
 
             // Prevent form submission
-            e.preventDefault();
+            //e.preventDefault();
 
             // Get the form instance
-            var $form = $(e.target);
+            //var $form = $(e.target);
 
             // Get the BootstrapValidator instance
-            var bv = $form.data('bootstrapValidator');
+            //var bv = $form.data('bootstrapValidator');
 
-             Use Ajax to submit form data
+             //Use Ajax to submit form data
             //$.post($form.attr('action'), $form.serialize(), function(result) {
             //    console.log(result);
             //}, 'json');
