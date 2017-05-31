60671f1aabbfba6a38a1237870274a17500d77ef
Carbery, Charlie <CCarbery@eab.com>
2017-05-31 16:55:23 -0400

    Stuff

--- js/index.js
+++ js/index.js
@@ -93,3 +93,24 @@
             }, 'json');
         }); */
 });
+$('#form').validator().on('submit', function (e) {
+	$('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
+
+    // Prevent form submission
+    e.preventDefault();
+
+    // Get the form instance
+    var $form = $(e.target);
+
+    // Get the BootstrapValidator instance
+    var bv = $form.data('bootstrapValidator');
+            
+    console.log($form.serialize());
+});
+
+
+
+
+
+
