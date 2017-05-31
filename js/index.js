b03303d369552e12bb97c3c6fb6cc311b4492ed8
Carbery, Charlie <CCarbery@eab.com>
2017-05-31 16:58:32 -0400

    Stuff

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
   $(document).ready(function() {
     $('#contact_form').bootstrapValidator({
         // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
         fields: {
             ethnicity: {
                 validators: {
